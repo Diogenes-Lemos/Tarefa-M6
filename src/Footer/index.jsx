62a5267cc522d9f6f9dcb1b3c71b6d7381d9e5cd
Diogenes-Lemos <diogenes.lemos1@gmail.com>
2025-03-25 09:05:47 -0300

    correção do Footer

--- src/Footer/index.jsx
+++ src/Footer/index.jsx
@@ -1,29 +1,33 @@
-import { Farea } from "./styles"
+import { Farea } from './styles'
 import Logo from '../assets/logo.png'
 import Instagram from '../assets/instagram.png'
 import Twitter from '../assets/twitter.png'
 import Facebook from '../assets/facebook.png'
 
 const Ftr = () => (
-    <Farea>
-        <div id="logoarea">
-            <img src={Logo} alt="logo" />
-        </div>
-        <div id="iconsarea">
-            <div id="icon1">
-                <img src={Instagram} alt="I" />
-            </div>
-            <div id="icon2">
-                <img src={Facebook} alt="I" />
-            </div>
-            <div id="icon3">
-                <img src={Twitter} alt="I" />
-            </div>
-        </div>
-        <div id="legal">
-            <p>A efood é uma plataforma para divulgação de estabelecimentos, a responsabilidade pela entrega, qualidade <br /> dos produtos é toda do estabelecimento contratado. </p>
-        </div>
-    </Farea>
+  <Farea>
+    <div id="logoarea">
+      <img src={Logo} alt="logo" />
+    </div>
+    <div id="iconsarea">
+      <div id="icon1">
+        <img src={Instagram} alt="I" />
+      </div>
+      <div id="icon2">
+        <img src={Facebook} alt="I" />
+      </div>
+      <div id="icon3">
+        <img src={Twitter} alt="I" />
+      </div>
+    </div>
+    <div id="legal">
+      <p>
+        A efood é uma plataforma para divulgação de estabelecimentos, a
+        responsabilidade pela entrega, qualidade <br /> dos produtos é toda do
+        estabelecimento contratado.{' '}
+      </p>
+    </div>
+  </Farea>
 )
 
 export default Ftr
